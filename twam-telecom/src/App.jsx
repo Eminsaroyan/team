3f921f2323c36cdb7adffad152284b380d782ccc
Emin Saroyan <emin.saroyan.ait@gmaile.com>
2025-06-03 15:03:07 +0400

    avelacrim kmaxq

--- twam-telecom/src/App.jsx
+++ twam-telecom/src/App.jsx
@@ -5,19 +5,20 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='flex space-between'>
-      <div className='flex'>
+    <div>
+      <div>
         <p>Անհատներին</p>
         <p>Բիզնես</p>
         <p>E-shop</p>
       </div>
-      <div className='flex'>
+      <div>
         <p>Հայ</p>
         <p>Рус</p>
         <p>Eng</p>
         <p>Անձնական գրասենյակ</p>
       </div>
     </div>
+
   )
 }
 
