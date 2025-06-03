c18e34262993429bcf86ca4826438c9b127ad2d1
Emin Saroyan <emin.saroyan.ait@gmaile.com>
2025-06-03 17:45:34 +0400

    mi qich dzeci css

--- twam-telecom/src/App.jsx
+++ twam-telecom/src/App.jsx
@@ -6,21 +6,21 @@ function App() {
 
   return (
     <>
-      <div className="h-[45px] flex justify-around bg-[#2c3843] text-[#ffffff]">
+      <div className="h-[45px] flex justify-around bg-[#01415f] text-[#ffffff]">
         <div className="flex items-center ">
-          <p className='m-[5px] p-[12px] hover:bg-[#455666] cursor-pointer transition-colors duration-1000'>Անհատներին</p>
-          <p className='m-[5px] p-[12px] hover:bg-[#455666] cursor-pointer transition-colors duration-1000'>Բիզնես</p>
-          <p className='m-[5px] p-[12px] hover:bg-[#455666] cursor-pointer transition-colors duration-1000'>E-shop</p>
+          <p className='m-[5px] p-[12px] hover:bg-[#0A6C9F] cursor-pointer transition-colors duration-1000'>Անհատներին</p>
+          <p className='m-[5px] p-[12px]  hover:bg-[#0A6C9F] cursor-pointer transition-colors duration-1000'>Բիզնես</p>
+          <p className='m-[5px] p-[12px] hover:bg-[#0A6C9F] cursor-pointer transition-colors duration-1000'>E-shop</p>
         </div>
         <div className="flex items-center ">
           <p className='m-[10px] border-l pl-[20px] border-r pr-[20px] border-[#757575] cursor-pointer hover:text-[#FF0000] transition-colors duration-1000'>Հայ</p>
           <p className='m-[10px] border-r pr-[20px] border-[#757575]  cursor-pointer hover:text-[#FF0000] transition-colors duration-1000'>Рус</p>
           <p className='m-[10px] border-r pr-[20px] border-[#757575]  cursor-pointer hover:text-[#FF0000] transition-colors duration-1000'>Eng</p>
-          <p className='m-[5px] p-[12px] cursor-pointer hover:bg-[#455666] transition-colors duration-1000'>Անձնական գրասենյակ</p>
+          <p className='m-[5px] p-[12px] cursor-pointer hover:bg-[#0A6C9F] transition-colors duration-1000'>Անձնական գրասենյակ</p>
         </div>
       </div>
       <nav className='flex justify-evenly items-center m-[10px]'>
-        <img src="https://www.telecomarmenia.am/img/logo.svg?v=1" alt="" className='cursor-pointer'/>
+        <img src="https://www.telecomarmenia.am/img/logo.svg?v=1" alt="" className='cursor-pointer' />
         <p className='cursor-pointer'>Սակագներ</p>
         <p className='cursor-pointer'>Ինտերնետ</p>
         <p className='cursor-pointer'>Ծառայություններ</p>
@@ -29,7 +29,14 @@ function App() {
         <p className='cursor-pointer'>Առաջարկներ</p>
         <p className='cursor-pointer'>Օգնություն</p>
       </nav>
-      
+      <div>
+        <div className="w-[500px] p-[60px] bg-[#01415f] ">
+          <p className=''>Բջջային կապ</p>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </>
   )
 }
