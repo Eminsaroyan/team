43a832c758335ad0539303dafd0c4072aec171f5
Emin Saroyan <emin.saroyan.ait@gmaile.com>
2025-06-03 19:17:04 +0400

    avelacrim css

--- twam-telecom/src/App.jsx
+++ twam-telecom/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
       <div className="grid grid-cols-2 justify-items-center gap-4 mt-[30px] m-[90px]">
         <div className="group flex justify-between items-start w-[600px] pl-[30px] bg-[#01415f] rounded-[12px] mb-[10px] cursor-pointer">
           <p className="text-[#ffffff] mt-[12px] text-[30px] font-[900] font-serif group-hover:text-[#FF0000] transition-colors duration-300">Բջջային կապ</p>
-          <img src="https://www.telecomarmenia.am/images/promo/1/16509682370213.png" alt="" className="rounded-[12px] object-cover"/>
+          <img src="https://www.telecomarmenia.am/images/promo/1/16509682370213.png" alt="" className="rounded-[12px] object-cover" />
         </div>
         <div className="group flex justify-between items-start w-[600px] pl-[30px] bg-[#01415f] rounded-[12px] mb-[10px] cursor-pointer">
           <p className="text-[#ffffff] mt-[12px] text-[30px] font-[900] font-serif group-hover:text-[#FF0000] transition-colors duration-300">Ֆիքսված կապ</p>
@@ -40,17 +40,17 @@ function App() {
         </div>
         <div className="group flex justify-between items-start w-[600px] pl-[30px] bg-[#01415f] rounded-[12px] cursor-pointer">
           <p className="text-[#ffffff] mt-[12px] text-[30px] font-[900] font-serif group-hover:text-[#FF0000] transition-colors duration-300">Հավելվածներ</p>
-          <img src="https://www.telecomarmenia.am/images/promo/1/16509690498367.png" alt="" className="rounded-[12px] object-cover"/>
+          <img src="https://www.telecomarmenia.am/images/promo/1/16509690498367.png" alt="" className="rounded-[12px] object-cover" />
         </div>
         <div className="group flex justify-between items-start w-[600px] pl-[30px] bg-[#01415f] rounded-[12px] cursor-pointer">
           <p className="text-[#ffffff] mt-[12px] text-[30px] font-[900] font-serif group-hover:text-[#FF0000] transition-colors duration-300">Ինտերնետ և TV</p>
-          <img src="https://www.telecomarmenia.am/images/promo/1/16510462295535.png" alt=""className="rounded-[12px] object-cover" />
+          <img src="https://www.telecomarmenia.am/images/promo/1/16510462295535.png" alt="" className="rounded-[12px] object-cover" />
         </div>
       </div>
-      <div>
-        <h1>Դարձեք Team բաժանորդ</h1>
-        <p>Միացե՛ք մեզ ձեր համարով և օգտվեք Team-ի բոլոր առավելություններից:</p>
-        <button>Միանալ</button>
+      <div className="flex flex-col items-center pt-[30px] pb-[70px] bg-[#01415f] text-[#ffffff] ">
+        <h1 className='text-[65px] mb-[12px] font-[900]'>Դարձեք Team բաժանորդ</h1>
+        <p className=' mb-[23px] font-[900] font-serif'>Միացե՛ք մեզ ձեր համարով և օգտվեք Team-ի բոլոր առավելություններից:</p>
+        <button className='pt-[15px] pb-[15px] pl-[100px] pr-[100px] bg-[#FF0000] font-[900] text-[15px] text-[#ffffff] rounded-[12px] border-none cursor-pointer'>Միանալ</button>
       </div>
     </>
   )
