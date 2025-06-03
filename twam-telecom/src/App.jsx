8620f9d34d1be331431c9b6f4acb5dcfdace68b1
Emin Saroyan <emin.saroyan.ait@gmaile.com>
2025-06-03 19:19:05 +0400

    css dzeci mi qich

--- twam-telecom/src/App.jsx
+++ twam-telecom/src/App.jsx
@@ -47,7 +47,7 @@ function App() {
           <img src="https://www.telecomarmenia.am/images/promo/1/16510462295535.png" alt="" className="rounded-[12px] object-cover" />
         </div>
       </div>
-      <div className="flex flex-col items-center pt-[30px] pb-[70px] bg-[#01415f] text-[#ffffff] ">
+      <div className="flex flex-col items-center pt-[30px] pb-[40px] bg-[#01415f] text-[#ffffff] ">
         <h1 className='text-[65px] mb-[12px] font-[900]'>Դարձեք Team բաժանորդ</h1>
         <p className=' mb-[23px] font-[900] font-serif'>Միացե՛ք մեզ ձեր համարով և օգտվեք Team-ի բոլոր առավելություններից:</p>
         <button className='pt-[15px] pb-[15px] pl-[100px] pr-[100px] bg-[#FF0000] font-[900] text-[15px] text-[#ffffff] rounded-[12px] border-none cursor-pointer'>Միանալ</button>
