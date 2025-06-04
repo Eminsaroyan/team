e4027e837fffe00a889ed081157477eb39dcecbe
Emin Saroyan <emin.saroyan.ait@gmaile.com>
2025-06-05 01:28:58 +0400

    avelacrim headeri css ev avatim pro

--- twam-telecom/src/App.jsx
+++ twam-telecom/src/App.jsx
@@ -88,44 +88,44 @@ function App() {
         </div>
         <img src="https://www.telecomarmenia.am/img/redesign/app-img.png" alt="" className='w-[30%]' />
       </div>
-      <div>
+      <div className='bg-[#01415f] flex text-[#ffffff] items-start justify-evenly pt-[30px] pb-[100px]'>
         <img src="https://www.telecomarmenia.am/img/logo-light.svg?v=1" alt="" />
-        <div>
-          <h5>Ընկերության մասին</h5>
-          <p>Մեր մասին</p>
-          <p>Կապի թանգարան</p>
-          <p>Նորություններ</p>
-          <p>Աշխատանք Տելեկոմ Արմենիայում</p>
-          <p>Արդյունքներ և հաշվետվություններ</p>
-          <p>Գործարար Էթիկա և Կոմպլայենս</p>
-          <p>Կայուն զարգացում</p>
-          <p>Բաժնետերերին</p>
+        <div className='leading-[32px] cursor-pointer'>
+          <h2 className='mb-[12px] text-[#c4c0c0]'>Ընկերության մասին</h2>
+          <p className='text-[14px] text-[#c4c0c0]'>Մեր մասին</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Կապի թանգարան</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Նորություններ</p>
+          <p className='text-[14px] text-[#c4c0c0] '>Աշխատանք Տելեկոմ Արմենիայում</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Արդյունքներ և հաշվետվություններ</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Գործարար Էթիկա և Կոմպլայենս</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Կայուն զարգացում</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Բաժնետերերին</p>
         </div>
-        <div>
-          <h5>Տեղեկատվություն</h5>
-          <p>Ընդհանուր դրույթներ և պայմաններ</p>
-          <p>Անվտանգություն</p>
-          <p>E-shop պայմաններ</p>
-          <p>Ապառիկ վաճառքի պայմաններ</p>
-          <p>Առաքման պայմաններ</p>
-          <p>Վաճառքի և սպասարկման կենտրոններ</p>
-          <p>Ծածկույթ</p>
-          <p>Բջջային ցանցի ծածկույթ</p>
-          <p>Team ինտերնետի հասանելիության ծածկույթ</p>
-          <p>Օգտակար փաստաթղթեր</p>
-          <p>Գործընկերներին և մատակարարներին</p>
-          <p>Գաղտնիության քաղաքականություն</p>
-          <p>ՀՀ մարզերի կոդեր</p>
+        <div className='leading-[30px] cursor-pointer'>
+          <h2 className='mb-[12px]'>Տեղեկատվություն</h2>
+          <p className='text-[14px text-[#c4c0c0]]'>Ընդհանուր դրույթներ և պայմաններ</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Անվտանգություն</p>
+          <p className='text-[14px] text-[#c4c0c0]'>E-shop պայմաններ</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Ապառիկ վաճառքի պայմաններ</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Առաքման պայմաններ</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Վաճառքի և սպասարկման կենտրոններ</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Ծածկույթ</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Բջջային ցանցի ծածկույթ</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Team ինտերնետի հասանելիության ծածկույթ</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Օգտակար փաստաթղթեր</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Գործընկերներին և մատակարարներին</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Գաղտնիության քաղաքականություն</p>
+          <p className='text-[14px] text-[#c4c0c0]'>ՀՀ մարզերի կոդեր</p>
         </div>
-        <div>
-          <h5>Team հավելվածներ</h5>
-          <p>TeamTV</p>
-          <p>My Team</p>
-          <p>Team Pay</p>
-          <p>Team Energy</p>
+        <div className='leading-[30px] cursor-pointer'>
+          <h2 className='mb-[12px]'>Team հավելվածներ</h2>
+          <p className='text-[14px] text-[#c4c0c0]'>TeamTV</p>
+          <p className='text-[14px] text-[#c4c0c0]'>My Team</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Team Pay</p>
+          <p className='text-[14px] text-[#c4c0c0]'>Team Energy</p>
         </div>
       </div>
-      <div>
+      <div className='flex justify-center bg-[#022739] text-[#ffffff] pt-[20px] pb-[20px]'>
         <p>Team © 2025</p>
       </div>
     </>
